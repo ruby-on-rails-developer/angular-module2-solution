56389e71fc2d7daa3a2330ee2f674a3872ed1694
Jean-Luc Bouthemy <jean-luc.b@logicrule.com>
2017-08-27 11:34:59 -0700

    button to be fixed

--- app.js
+++ app.js
@@ -3,13 +3,50 @@
 
   angular.module('ShoppingListCheckOff', [])
   .controller('ToBuyController', ToBuyController)
-  .controller('AlreadyBoughtController', AlreadyBoughtController);
+  .controller('AlreadyBoughtController', AlreadyBoughtController)
+  .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-  ToBuyController.$inject = ['$scope'];
-  function ToBuyController($scope) {
+  ToBuyController.$inject = ['ShoppingListCheckOffService'];
+  function ToBuyController(ShoppingListCheckOffService) {
+    var showListToBuy = this;
+
+    showListToBuy.itemsToBuy = ShoppingListCheckOffService.getItemsToBuy();
+    showListToBuy.boughtItem = function(index) {
+      ShoppingListCheckOffService.boughtItem(index);
+    };
+    showListToBuy.isEmpty = ShoppingListCheckOffService.isListToBuyEmpty();
   }
 
-  AlreadyBoughtController.$inject = ['$scope'];
-  function AlreadyBoughtController($scope) {
+  AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+  function AlreadyBoughtController(ShoppingListCheckOffService) {
+    var showListBought = this;
+
+    showListBought.itemsBought = ShoppingListCheckOffService.getItemsBought();
+    showListBought.isEmpty = ShoppingListCheckOffService.isListBoughtEmpty();
+  }
+
+  function ShoppingListCheckOffService() {
+    var service = this;
+
+    var buy = [
+      { name: "cookies", quantity: 10 },
+      { name: "oranges", quantity: 5  },
+      { name: "bananas", quantity: 8  },
+      { name: "stamps",  quantity: 3 },
+      { name: "eggs",    quantity: 12 }
+    ];
+    var bought = [];
+
+    service.getItemsToBuy = function () { return buy; };
+    service.isListToBuyEmpty = function () { return buy[0] === undefined; };
+
+    service.getItemsBought = function () { return bought; };
+    service.isListBoughtEmpty = function () { return bought[0] === undefined; };
+    service.BoughtItem = function(itemIndex) {
+      var item = service.buy[itemIndex];
+
+      service.buy.splice(itemIndex,1);
+      service.bought.push(item);
+    };
   }
 })();
